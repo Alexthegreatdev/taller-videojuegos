0004f8fca4256990ec9bbbd5ca313aa569689465
alexander <axldeth@gmail.com>
2022-09-28 15:52:20 -0400

    Render de mapas

--- game.js
+++ game.js
@@ -1,34 +1,34 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
+let canvasSize;
+let elementSize;
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
-window.addEventListener('load', startGame);
-window.addEventListener('resize', startGame);
-
-
-function startGame() {
-
-    let canvasSize = window.innerHeight > window.innerWidth ? window.innerWidth * 0.8 : window.innerHeight * 0.8;
-
+function setCanvasSize() {
+    canvasSize = window.innerHeight > window.innerWidth ? Math.floor(window.innerWidth * 0.8) : Math.floor(window.innerHeight * 0.8);
+    
     canvas.setAttribute('width', canvasSize);
     canvas.setAttribute('height', canvasSize);
+    
+    elementSize = Math.floor(canvasSize / 10);
+    startGame()
+}
 
-    const elementSize = canvasSize / 10;
-
+function startGame() {
     console.log({elementSize, canvasSize});
 
     game.font = elementSize + 'px Verdana';        
-    game.textAlign = 'end';        
-    for (let i = 1; i <= 10; i++) {
-        game.fillText(emojis['X'], elementSize * i, elementSize);
-        
+    game.textAlign = 'end';  
+    
+    const map = maps[0];
+    const mapRows = map.trim().split('\n')
+    const mapCols = mapRows.map(row => row.trim().split(''))
+    console.log(map, mapRows, mapCols);
+
+    for (let row = 1; row <= 10; row++) {
+        for (let col = 1; col <= 10; col++) {
+            game.fillText(emojis[mapCols[row - 1][col - 1]], elementSize * col , elementSize * row );    
+        }
     }
-
-
-    // game.fillRect(0, 50, 100, 100);
-    // game.clearRect(50, 50, 50, 50);
-    // game.font = '25px Verdana';
-    // game.fillStyle = 'purple';
-    // game.textAlign = 'center';
-    // game.fillText('text',25 ,25);
-
 }
